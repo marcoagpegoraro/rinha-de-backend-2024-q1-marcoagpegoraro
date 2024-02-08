dc3470c87c7da1696b5d5412d5f0fb21e39600fe
Marco Antonio <marco@Marcos-MacBook-Air.local>
2024-02-07 23:21:50 -0300

    created the two routes of the challenge

--- main.v
+++ main.v
@@ -1,5 +1,37 @@
 module main
 
+import vweb
+
+struct App {
+	vweb.Context
+}
+
+struct Teste {
+	nome string
+	idade i16
+}
+
 fn main(){
-	print("Hello, world")
+	app := App{}
+	vweb.run(app, 8080)
+}
+
+@['/']
+pub fn (mut app App) teste() vweb.Result {
+	teste := Teste{
+		nome : "Marco"
+		idade : 24
+	}
+	return app.json(teste)
+}
+
+
+@['/clientes/:id/transacoes'; post]
+pub fn (mut app App) post_transacao(id i64) vweb.Result {
+	return app.text('Id recebido $id')
+}
+
+@['/clientes/:id/extrato'; get]
+pub fn (mut app App) get_extrato(id i64) vweb.Result {
+	return app.text('Id recebido $id')
 }
