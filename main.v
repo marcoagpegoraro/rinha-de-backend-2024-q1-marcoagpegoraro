3eed535691976788271ce570a7203f2375ff4711
Marco Antonio <marco@Marcos-MacBook-Air.local>
2024-02-11 15:13:57 -0300

    create tables working

--- main.v
+++ main.v
@@ -1,34 +1,90 @@
 module main
 
 import vweb
+import db.pg
+import json 
+// import time 
 
 struct App {
 	vweb.Context
+	db_handle vweb.DatabasePool[pg.DB] = unsafe { nil }
+pub mut:
+	db pg.DB
 }
 
-struct Teste {
-	nome string
-	idade i16
+@[table: 'transacao']
+struct Transacao {
+	id        		int     @[primary; sql: serial]
+	id_cliente		int
+	tipo 			string
+	valor 			i64
+	descricao 		string
+	realizada_em  	string   @[default: 'CURRENT_TIME']
+}
+
+
+@[table: 'cliente']
+struct Cliente {
+	id        		int     @[primary; sql: serial]
+	limite			i64 
+	saldo_inicial	i64 
+	transacoes    []Transacao     @[fkey: 'id']
+}
+
+struct ExtratoResponseDto {
+	saldo SaldoDto
+	transacao []TransacaoDto
+}
+
+struct SaldoDto {
+	total i64
+	data_extrato string
+	limite i64
+}
+
+struct TransacaoDto {
+	valor i64
+	tipo string
+	descricao string
+	realizada_em string
+}
+
+fn get_database_connection() pg.DB {
+	return pg.connect(user: 'postgres', password: '12345', dbname: 'postgres') or { panic(err) }
 }
 
 fn main(){
-	app := App{}
+
+	db := get_database_connection()
+
+	pool := vweb.database_pool(handler: get_database_connection)
+
+	sql db {
+		create table Transacao
+	}!
+	sql db {
+		create table Cliente
+	}!
+
+	app := &App{
+		db_handle: pool
+	}
+
 	vweb.run(app, 8080)
 }
 
-@['/']
-pub fn (mut app App) teste() vweb.Result {
-	teste := Teste{
-		nome : "Marco"
-		idade : 24
-	}
-	return app.json(teste)
-}
-
-
 @['/clientes/:id/transacoes'; post]
 pub fn (mut app App) post_transacao(id i64) vweb.Result {
-	return app.text('Id recebido $id')
+	body := json.decode(TransacaoDto, app.req.data) or {
+		app.set_status(400, '')
+		return app.text('Failed to decode json, error: $err')
+	}
+
+	// app.db_handle.
+
+
+	// return app.text('Id recebido $id')
+	return app.json(body)
 }
 
 @['/clientes/:id/extrato'; get]
